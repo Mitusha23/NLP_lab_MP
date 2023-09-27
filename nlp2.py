ab658c783b219ac20f006af9d8bf5c79ebf7fd62
Mitusha23 <113438721+Mitusha23@users.noreply.github.com>
2023-09-27 11:18:37 +0530

    output added

--- nlp2.py
+++ nlp2.py
@@ -35,4 +35,21 @@ g_tfidf = models.TfidfModel(g_bow, smartirs='ntc')
 
 print("TF-IDF Vector:")
 for item in g_tfidf[g_bow]:
-    print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item]
+    print([[g_dict[id], np.around(freq, decimals=2)] for id, freq in item])
+
+# Output: 
+#    The dictionary has: 13 tokens
+
+# {'Mitusha': 0, 'My': 1, 'Pawar.': 2, 'is': 3, 'name': 4, 'College': 5, 'Engineering.': 6, 'I': 7, 'Sanjivani': 8, 'am': 9, 'from': 10, 'of': 11, 'Ahmednagar.': 12}
+# Bag of Words :  [[(0, 1), (1, 1), (2, 1), (3, 1), (4, 1)], [(5, 1), (6, 1), (7, 1), (8, 1), (9, 1), (10, 1), (11, 1)], [(0, 1), (3, 1), (10, 1), (12, 1)]]
+
+# -----------------------------------------
+# -------------------TFIDF----------------------
+# Dictionary : 
+# [['is', 1], ['mitusha', 1], ['my', 1], ['name', 1], ['pawar', 1]]
+# [['am', 1], ['college', 1], ['engineering', 1], ['from', 1], ['of', 1], ['sanjivani', 1]]
+# [['am', 1], ['from', 1], ['department', 1], ['information', 1], ['technology', 1]]
+# TF-IDF Vector:
+# [['is', 0.45], ['mitusha', 0.45], ['my', 0.45], ['name', 0.45], ['pawar', 0.45]]
+# [['am', 0.24], ['college', 0.47], ['engineering', 0.47], ['from', 0.24], ['of', 0.47], ['sanjivani', 0.47]]
+# [['am', 0.27], ['from', 0.27], ['department', 0.53], ['information', 0.53], ['technology', 0.53]]
